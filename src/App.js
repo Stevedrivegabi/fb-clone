e73b4f7fa0efec4bbcdb7139ca71446f64e4ab55
stevedrivegabi <gabriel_tillema@hotmail.com>
2021-01-12 00:31:21 +0100

    Build the Navigation Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './Header.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build the facebook clone!</h1>
+      <Header />
+
+      <div className="app__body">
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search"
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar, IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+            <img src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo.jpg" 
+                 alt="Facebook logo"/>
+            </div>
+            <div className="header__input">
+                <SearchIcon />
+                <input placeholder="Search Facebook " type="text"/>
+            </div>
+
+            <div className="header__middle">
+                <div className="header__option header__option--active">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <StorefrontIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large" />
+                </div>
+            </div>
+
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Gabriel Tillema</h4>
+
+                    <IconButton>
+                        <AddIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ForumIcon />
+                    </IconButton>
+                    <IconButton>
+                        <NotificationsActiveIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ExpandMoreIcon />
+                    </IconButton>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
