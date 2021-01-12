b702279e8b21f3a1371ced112ceee97f20fe7704
stevedrivegabi <gabriel_tillema@hotmail.com>
2021-01-12 14:30:09 +0100

    Made the Story section

---
+++ src/StoryReel.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Story from './Story.js';
+import './StoryReel.css';
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story 
+                image="https://www.sohh.com/wp-content/uploads/Screen-Shot-2016-09-13-at-1.54.19-PM.png"
+                profileSrc="https://i.guim.co.uk/img/media/c1f430159c3e7e58c9b46098efde3dee7ba7d830/0_196_2515_1509/master/2515.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f554bca9c12ca49c79264bce18b42472"
+                title="Tupac Shakur"
+            />
+            <Story 
+                image="https://www.sohh.com/wp-content/uploads/Screen-Shot-2016-09-13-at-1.54.19-PM.png"
+                profileSrc="https://i.guim.co.uk/img/media/c1f430159c3e7e58c9b46098efde3dee7ba7d830/0_196_2515_1509/master/2515.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f554bca9c12ca49c79264bce18b42472"
+                title="Tupac Shakur"
+            />
+            <Story 
+                image="https://www.sohh.com/wp-content/uploads/Screen-Shot-2016-09-13-at-1.54.19-PM.png"
+                profileSrc="https://i.guim.co.uk/img/media/c1f430159c3e7e58c9b46098efde3dee7ba7d830/0_196_2515_1509/master/2515.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f554bca9c12ca49c79264bce18b42472"
+                title="Tupac Shakur"
+            />  
+            <Story 
+                image="https://www.sohh.com/wp-content/uploads/Screen-Shot-2016-09-13-at-1.54.19-PM.png"
+                profileSrc="https://i.guim.co.uk/img/media/c1f430159c3e7e58c9b46098efde3dee7ba7d830/0_196_2515_1509/master/2515.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f554bca9c12ca49c79264bce18b42472"
+                title="Tupac Shakur"
+            />
+            <Story 
+                image="https://www.sohh.com/wp-content/uploads/Screen-Shot-2016-09-13-at-1.54.19-PM.png"
+                profileSrc="https://i.guim.co.uk/img/media/c1f430159c3e7e58c9b46098efde3dee7ba7d830/0_196_2515_1509/master/2515.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f554bca9c12ca49c79264bce18b42472"
+                title="Tupac Shakur"
+            />                    
+        </div>
+    );
+}
+
+export default StoryReel
