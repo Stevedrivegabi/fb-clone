484e4877dc207f73890e35a627c0b3732dc4b7a3
stevedrivegabi <gabriel_tillema@hotmail.com>
2021-01-12 01:10:15 +0100

    Created the sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+import { Chat, EmojiFlags, ExpandMoreOutlined, LocalHospital, People, Storefront, VideoLibrary } from '@material-ui/icons';
+import React from 'react';
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow.js";
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+           <SidebarRow src="https://website.storage/Data/DUNO/RTE/Afbeeldingen/NieuwsItem/12683/ws3.jpg" 
+           title="Gabriel Tillema" /> 
+           <SidebarRow 
+            Icon={LocalHospital}
+            title="COVID-19 Information Center"
+            />
+           <SidebarRow Icon={EmojiFlags}title="Pages"/>
+           <SidebarRow Icon={People}title="Friends"/>
+           <SidebarRow Icon={Chat}title="Messenger"/>
+           <SidebarRow Icon={Storefront}title="Marketplace"/>
+           <SidebarRow Icon={VideoLibrary}title="Videos"/>
+           <SidebarRow Icon={ExpandMoreOutlined}title="Marketplace"/>
+        </div>
+    )
+}
+
+export default Sidebar
